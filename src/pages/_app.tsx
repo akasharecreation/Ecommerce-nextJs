f98db6a389b66a7b85ca9adbd32ce06e1e21b8de
akasharecreation <akashare-creationgraphics@outlook.com>
2022-02-21 19:28:38 -0300

    atualizacao-packa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
+import { store } from '../app/store'
+import { Provider } from 'react-redux'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Provider store={store}>
+    <Component {...pageProps} />
+    </Provider>
+  ) 
 }
 
 export default MyApp
