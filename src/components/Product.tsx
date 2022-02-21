3bdc63183dc7d2ad8c7fa65a22017f2d8cf7a409
akasharecreation <akashare-creationgraphics@outlook.com>
2022-02-21 17:14:05 -0300

    atualização

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -6,7 +6,7 @@ import Currency from "react-currency-formatter"
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
-function Product({ title, price, description, category, image }) {
+function Product({ id, title, price, description, category, image }) {
   const [rating] = useState(
       Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   )
